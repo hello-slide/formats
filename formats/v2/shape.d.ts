f2bd645eced593a0b1414407d2bb3e2009d4b74f
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-27 17:53:55 +0900

    added shapes

---
+++ formats/v2/shape.d.ts
@@ -0,0 +1,5 @@
+import {Line, Box, Circle} from './shapes';
+
+export interface Shape {
+  shape: Line | Box | Circle;
+}
---
+++ formats/v2/shapes/index.d.ts
@@ -0,0 +1,3 @@
+export type {Line} from './line';
+export type {Box} from './box';
+export type {Circle} from './circle';
