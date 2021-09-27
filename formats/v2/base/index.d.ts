20aef7e65afcb36855b466f653c17a93118448f1
Yuto Watanabe <yuto.w51942@gmail.com>
2021-09-27 17:52:12 +0900

    added base

---
+++ formats/v2/base/index.d.ts
@@ -0,0 +1,5 @@
+export type {Color} from './color';
+export type {Image} from './image';
+export type {Metadata} from './metadata';
+export type {Text, TextFormat} from './text';
+export type {Graph, GraphElement} from './graph';
